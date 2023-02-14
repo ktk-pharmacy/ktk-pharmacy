2f31b2357efa30a151ae6eb8698a9db62fb47871
Zin Mar Kyaw <zinmark4gen@gmail.com>
2023-02-14 17:39:57 +0630

    first commit

---
+++ resources/views/Layouts/headers.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="TemplateMo">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <title>KTK Pharmacy</title>
+
+    <!-- Bootstrap core CSS -->
+ 
+    <link href="{{asset('assets/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-edu-meeting.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+    <style type="text/css">
+        /* .img {
+            content: url("/path/here/to/image.png");
+        } */
+
+    </style>
+<!--
+
+
+-->
+  </head>
+
+<body>
+
+  <!-- Sub Header -->
+  <div class="sub-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-sm-8">
+          <div class="left-content">
+            <!-- <p>This is an Pharmacy  <em>HTML CSS</em> template by TemplateMo website.</p> -->
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-4">
+          <div class="right-icons">
+            <ul>
+              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa fa-behance"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+      <div class="container">
+          <div class="row">
+              <div class="col-12">
+                  <nav class="main-nav">
+                      <!-- ***** Logo Start ***** -->
+                      <a href="{{url('home')}}" class="logo">
+                            <!-- {{asset('assets/images/ktk_icon_3tp.png')}} -->
+                            <img width="50" height="100" class="img" src="" alt="">
+                            <!-- <img width="50" height="100" alt=""> -->
+                      </a>
+                      <!-- ***** Logo End ***** -->
+                      <!-- ***** Menu Start ***** -->
+                      <ul class="nav">
+                          <li class="scroll-to-section"><a href="{{url('home')}}" class="active">Home</a></li>
+                          <li><a href="meetings.html">About Us</a></li>
+                          <li class="has-sub">
+                              <a href="javascript:void(0)">Products</a>
+                              <ul class="sub-menu">
+                                  <li><a href="meetings.html">Medicine</a></li>
+                                  <li><a href="meeting-details.html">Consumer</a></li>
+                                  <li><a href="meeting-details.html">Beauty & Wellness</a></li>
+                              </ul>
+                          </li>
+                          <li class="scroll-to-section"><a href="#courses">News & Blog</a></li> 
+                          <li class="has-sub">
+                              <a href="javascript:void(0)">Contact Us</a>
+                              <ul class="sub-menu">
+                                  <li><a href="meetings.html">Contact Us</a></li>
+                                  <li><a href="meeting-details.html">CSR</a></li>
+                              </ul>
+                          </li>
+                      </ul>        
+                      <a class='menu-trigger'>
+                          <span>Menu</span>
+                      </a>
+                      <!-- ***** Menu End ***** -->
+                  </nav>
+              </div>
+          </div>
+      </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
