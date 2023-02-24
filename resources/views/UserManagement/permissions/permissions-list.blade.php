3a3d82c4b455be6ee6a2598386d55f14170f827f
Zin Mar Kyaw <zinmark4gen@gmail.com>
2023-02-24 14:45:15 +0630

    Permission Added

--- resources/views/UserManagement/permissions/permissions-list.blade.php
+++ resources/views/UserManagement/permissions/permissions-list.blade.php
@@ -5,7 +5,7 @@
             <div class="card-body tb-card">
                 <h4 class="card-title">User List</h4>
                 <div class="col-lg-2 mb-7 right py-4 flex ml-auto">
-                    <a type="button" class="btn btn-primary btn-icon-text bg-green" href="{{ route('register') }}">
+                    <a type="button" class="btn btn-primary btn-icon-text bg-green" href="{{route('permission_create')}}">
                         <i class="mdi mdi-file-check btn-icon-prepend"></i> Add New
                     </a>
                 </div>
@@ -25,10 +25,10 @@
                                     <td>{{ $key += 1 }}</td>
                                     <td>{{ $permission->name }}</td>
                                     <td>
-                                        <a href=""
+                                        <a href="{{route('permission_edit',$permission->id)}}"
                                             class="mx-2"><i class="fa-regular fa-pen-to-square"></i></a>
                                         <a href="javascript:void(0)"
-                                            data-url=""
+                                            data-url="{{route('permission_destroy',$permission->id)}}"
                                             class="text-danger delete-btn"><i class="fa-solid fa-square-xmark"></i></a>
                                     </td>
                                 </tr>
--- routes/user-management.php
+++ routes/user-management.php
@@ -8,4 +8,7 @@ Route::controller(PermissionController::class)
         Route::get('/permission_list', 'permission_list')->name('permission_list');
         Route::get('/permission_create', 'create')->name('permission_create');
         Route::post('/permission_create', 'store')->name('permission_store');
+        Route::get('/permission_edit/{permission}', 'edit')->name('permission_edit');
+        Route::patch('/permission_edit/{permission}', 'update')->name('permission_update');
+        Route::delete('/permission_delete/{permission}', 'destroy')->name('permission_destroy');
     });
