bc3952b6805f1873e892e4a2aafa790e264b75db
MinnShinSawLingMyat <lma@minshinsaw.com>
2024-05-11 15:24:05 +0630

    Footer UI

--- resources/views/frontend/Layouts/footer.blade.php
+++ resources/views/frontend/Layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer style="margin-top: auto;">
     <!-- Sub Header -->
-    <div style="background-color: #CC191D" class="sub-header">
+    {{-- <div style="background-color: #CC191D" class="sub-header">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-sm-8">
@@ -12,8 +12,10 @@
 
                     <div class="right-icons">
                         <ul>
-                            <li><a href="{{ site_settings()['social_facebook'] }}"><i
-                                        class="fa-brands fa-facebook"></i></a>
+                            <li>
+                                <a href="{{ site_settings()['social_facebook'] }}">
+                                    <img style="width:40px !important" src="{{asset('assets/images/fb.png')}}" alt="">
+                                </a>
                             </li>
                             <li><a href="{{ site_settings()['social_twitter'] }}"><i
                                         class="fa-brands fa-twitter"></i></a>
@@ -28,29 +30,77 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
     <section class="footer">
-        <div class="footer">
-            <div class="content-left">
-                <p>
-                    <img class="footerimage" src="/assets/images/phone.png" alt="">
-                    {{ site_settings()['default_phone_number'] }}
-                </p>
-                <p>
-                    <img class="footerimage" src="/assets/images/email.png" alt="">
-                    {{ site_settings()['default_email'] }}
-                </p>
-                <p>
-                    <img class="footerimage" src="/assets/images/home.png" alt="">
-                    {{ site_settings()['default_address'] }}
-                </p>
-                <p>
-                    <img class="footerimage" src="/assets/images/webpage.png" alt="">
-                    {{ site_settings()['site_url'] }}
-                </p>
+        <div class="pt-4 px-5">
+
+            <div class="row gap-3 text-start">
+                <div class="col-4">
+                    <a class="text-white mb-2" href="">ABOUT US</a>
+
+                    <p>
+                        <b>MISSION</b>
+                        {{-- <small>{{ site_settings()['mission'] }}</small> --}}
+                    </p>
+
+                    <p>
+                        <b>VISSION</b>
+                        {{-- <small>{{ site_settings()['mission'] }}</small> --}}
+                    </p>
+                    <p>
+                        <b>CORE VALUE</b>
+                        {{-- <small>{{ site_settings()['mission'] }}</small> --}}
+                    </p>
+                </div>
+                <div class="col-5">
+                    <p class="mb-2">CONTACT INFO</p>
+
+                    <p>{{ site_settings()['default_address'] }}</p>
+
+                    <p>
+                        <b>EMAIL &#160;&#160;:</b>
+                        <a class="text-white" href="mailto:{{ site_settings()['default_email'] }}">
+                            {{ site_settings()['default_email'] }}
+                        </a>
+                    </p>
+
+                    <p>
+                        <b>PHONE :</b>
+                        <a class="text-white" href="tel:+{{ site_settings()['default_phone_number'] }}">
+                            {{ site_settings()['default_phone_number'] }}
+                        </a>
+                    </p>
+                </div>
+                <div class="col-1">
+                    <p class="mb-2">FOLLOW US ON</p>
+
+                    <div class="right-icons">
+                        <ul class="d-flex gap-2">
+                            <li>
+                                <a href="{{ site_settings()['social_facebook'] }}">
+                                    <img style="width:40px !important" src="{{asset('assets/images/fb.png')}}" alt="">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ site_settings()['social_twitter'] }}">
+                                    <img style="width:40px !important" src="{{asset('assets/images/twitter.png')}}" alt="">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ site_settings()['social_instagram'] }}">
+                                    <img style="width:40px !important" src="{{asset('assets/images/insta.png')}}" alt="">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ site_settings()['social_linkedin'] }}">
+                                    <img style="width:40px !important" src="{{asset('assets/images/link.png')}}" alt="">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <p>{{ site_settings()['footer_copyright_text'] }}</p>
-            <!-- <br>Design: <a href="https://templatemo.com" target="_parent" title="free css templates">TemplateMo</a></p> -->
+            <p class="mt-3">{{ site_settings()['footer_copyright_text'] }}</p>
         </div>
     </section>
 </footer>
